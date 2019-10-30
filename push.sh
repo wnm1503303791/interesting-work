9bbff610da46355cd2b072e6c7dcd283208300dd
wnm1503303791 <wnm1503303791@gmail.com>
2019-10-30 16:17:04 +0800

    2019-10-30

--- push.sh
+++ push.sh
@@ -5,6 +5,19 @@
 # Created Time: 2019年09月11日 星期三 19时56分03秒
 #########################################################################
 #!/bin/bash
+
+if [ $# -eq 0 ]
+then
+    pushmessage=`date +%Y-%m-%d`
+else
+    pushmessage="$*"
+fi
+
+#pushmessage=`date +%Y-%m-%d`
+
+echo ${pushmessage}
+
 git add -A
-git commit -m "$*"
+git commit -m "${pushmessage}"
 git push origin master
+
